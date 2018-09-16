b67ec92545fe1f15ab4813dbb4eca3f3531c2984
Tyler G <guthriet1230@gmail.com>
2018-09-16 10:51:51 -0500

    basic javascript complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,135 @@
-var car = {
+var wins = 0;
+var loses = 0;
 
-    // Properties of our car object.
-    make: "Honda",
-    model: "Fit",
-    color: "Blue Raspberry",
-    mileage: 3000,
-    isWorking: true,
+// develop arrays for each letter and word
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "V", "w", "X", "y", "z"]
+var wordList = ["fairy", "shrek", "snow white", "seven dwarfs", "prince charming", "simba", "mustafa", "mickey mouse", "griffin", "centaur", "siren", "minotaur", "chimera", "phoenix", "unicorn", "leprechaun", "donald duck", "elsa", "princess jasmine", "buzz lightyear", "cinderella", "tinker bell", "peter pan", "alice in wonderland", "cheshire cat"];
 
-    // Creating the driveToWork method.
-    // A method is a function associated with an object.
-    driveToWork: function () {
+// develop arrays for the right word and wrong guesses
 
-        // When we say "this" (as in this.mileage) we are referring to the object the method is a part of.
-        // In this case, we will alert the mileage of the car object.
-        alert("Old Mileage: " + this.mileage);
+var rightGuess = [];
+var wrongGuess = [];
 
-        // Adding 8 to the car's mileage.
-        this.mileage = this.mileage + 8;
+// display underscores
+var underScore =[];
 
-        // Alerting the car's new mileage.
-        alert("New mileage: " + this.mileage);
-    },
+// choose a random word
+var random = Math.floor((Math.random() * wordList.length));
+var chosenWord = wordList[random];
+console.log(chosenWord);
 
-    // The driveAroundWorld method adds 24,000 miles to our car, sets isWorking to false, and makes some alerts.
-    driveAroundWorld: function () {
 
-        alert("Old Mileage: " + this.mileage);
-
-        this.mileage = this.mileage + 24000;
-
-        alert("New Mileage: " + this.mileage);
-        alert("Car needs a tuneup!");
-
-        this.isWorking = false;
-    },
-
-    // The getTuneUp method sets isWorking to true and alerts a message.
-    getTuneUp: function () {
-        alert("Car is ready to go!");
-        this.isWorking = true;
-    },
-
-    // The honk method alerts a honking message.
-    honk: function () {
-        alert("Honk! Honk!");
-    },
-
-    //   console.logStats()
-    logStats: function () {
-        console.log(car);
-    }
-    //^this is a method
-};
-
-//-----------------------
-//event handler for key up
-// -----------------------
-
-document.onkeyup = function (event) {
-    console.log(event.key);
-
-    // if h then honk - log stats
-    if (event.key === "h") {
-        car.honk();
-        car.logStats();
-    }
-
-    // if d then drive to work - log stats
-    if (event.key === "d") {
-        car.driveToWork();
-        car.logStats();
-    }
-
-    // if w thend rive around the world - log stats
-    if (event.key === "w") {
-        car.driveAroundWorld();
-        car.logStats();
-    }
-
-    // if t then tune up - log stats
-    if (event.key === "t") {
-        car.getTuneUp();
-        car.logStats();
-    }
+// underscores our random word
+function startup(){
+for (var i =0; i < chosenWord.length; i++){
+    underScore.push("_ ");
 }
+    return underScore;
+}
+console.log(startup());
+
+// var underscore = document.getElementById("word-choice");
+// underscore.textContent = "";
+
+
+
+// EVENT
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    console.log(userGuess);
+}
+   
+   
+// IF RIGHT GUESS, PUSH TO RIGHT ARRAY
+    if (userGuess.indexOf(chosenWord) > -1){ 
+        rightGuess.push(userGuess);
+        console.log(rightGuess);
+    }
+
+// // IF WRONG GUESS, PUSH TO WRONG ARRAY
+//     else {
+//         wrongGuess.push(keyword);
+//         console.log(wrongGuess);
+//     }
+    
+
+
+
+
+
+
+
+
+// if userGuess == alphabet display 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // display the random word
+// chosenWord.textContent = wordList[i];
+
+// // identifying where the messages get displayed
+// var numbGuess = document.getElementById("numb-guesses");
+// var lettersGuessed = document.getElementById("letters-guessed");
+// var winsText = document.getElementById("score-wins");
+// var lossesText = document.getElementById("score-loses");
+// var wordChoice = document.getElementById("word-choice");
+
+// // user input (event key)
+
+// document.onkeyup = function (event) {
+//     console.log(event.key);
+// }
+
+// // take user input and check it against the specific word
+
+
+// // develop a loop to run through all of the arrays
+
+
+// for (var i = 0; i < wordList.length; i++) {
+//     console.log("I love " + chosenWord[i]);
+//   }
+
+//   for (var i = 0; i < wordList.length; i++) {
+//     console.log("I love " + chosenWord[i]);
+//   }
+
+// // numbGuess.textContent - only changing text of the screen
+// // numbGuess.innerHTML - only changing text of the screen
+
+
+// var astericksSpan = document.getElementById("astericks");
+// var astericksWord = "";
+
+
+// for (var i = 0; i < wordList.length; i++) {
+//     console.log("I love " + chosenWord[i]);
+//   }
+
+
+
+
+
+// // Display the user and computer guesses, and wins/losses/ties.
+//     userChoiceText.textContent = "You chose: " + userGuess;
+//     computerChoiceText.textContent = "The computer chose: " + computerGuess;
+//     winLossTie.textContent = "that was a " + result;
+//     winsText.textContent = "wins: " + wins;
+//     lossesText.textContent = "losses: " + losses;
+//     tiesText.textContent = "ties: " + ties;
